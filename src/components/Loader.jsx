70bf6ef773bd0675eaad3a795e486e564bd6260e
Khryz <christianvalentevargas110898@gmail.com>
2025-12-11 22:06:19 -0600

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import "./Loader.css";
 export default function Loader() {
   return (
     <div className="loader-container">
-        <img src="https://drive.google.com/file/d/1H6NmdjmanYEkDMahjc4fYZhAVwzgS7bJ/view?usp=sharing" alt="logo" className="spin-logo" />
+        <img src="https://onuris-my.sharepoint.com/:i:/r/personal/1029221_onuriscp_com/Documents/Imagenes/Logo_700x700.png?csf=1&web=1&e=Qbfa0I" alt="logo" className="spin-logo" />
           
     </div>
   );
